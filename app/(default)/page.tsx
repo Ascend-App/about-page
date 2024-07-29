01bad886ffde6a216b37ff58f4efe1f5d5649651
Colin Graydon <colingraydon@gmail.com>
2024-07-29 16:29:46 -0400

    docs: add terms and conditions

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,6 +8,7 @@ import Features from "@/components/features";
 import Newsletter from "@/components/newsletter";
 import Zigzag from "@/components/zigzag";
 import Testimonials from "@/components/testimonials";
+import Footer from "@/components/ui/footer";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <Zigzag />
       <Testimonials />
       <Newsletter />
+      <Footer />
     </>
   );
 }
